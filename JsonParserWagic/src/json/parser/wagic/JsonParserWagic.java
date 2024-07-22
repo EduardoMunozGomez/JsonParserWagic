298892c03b100ae4802f0c8245e194d3806c06f5
Eduardo MG <punkeduard@gmail.com>
2024-07-21 22:24:46 -0600

    Preparing for BLB

--- JsonParserWagic/src/json/parser/wagic/JsonParserWagic.java
+++ JsonParserWagic/src/json/parser/wagic/JsonParserWagic.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,67 +15,75 @@ import org.json.simple.parser.ParseException;
 // @author Eduardo
 public class JsonParserWagic {
 
-    private static final String setCode = "ONE";
-    private static String filePath = "C:\\Users\\Eduardo_\\Downloads\\MTGJSON\\" + setCode;
+    private static final String setCode = "BLB";
+    private static final String filePath = "C:\\Users\\Eduardo_\\Downloads\\MTGJSON\\" + setCode;
 
     public static String getFilePath() {
         return filePath;
     }
 
-    public static void setFilePath(String aFilePath) {
-        filePath = aFilePath;
-    }
-
     public static void main(String[] args) {
-
-        boolean createCardsDat = false;
 
         File directorio = new File(getFilePath());
         directorio.mkdir();
 
-        try {
-            FileReader reader = new FileReader(getFilePath() + ".json");
-            File myObj = new File(getFilePath() + "\\_cards.dat");
-            myObj.createNewFile();
-            FileWriter myWriter;
-            myWriter = new FileWriter(myObj.getCanonicalPath());
-            FileWriter myWriterImages;
-            myWriterImages = new FileWriter("C:\\Users\\Eduardo_\\Downloads\\MTGJSON\\image.cvs", true);
+        try ( FileReader reader = new FileReader(getFilePath() + ".json", StandardCharsets.UTF_8);  FileWriter myWriter = new FileWriter(getFilePath() + "\\_cards.dat");  FileWriter myWriterImages = new FileWriter("C:\\Users\\Eduardo_\\Downloads\\MTGJSON\\image.cvs", true)) {
 
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(reader);
             JSONObject data = (JSONObject) jsonObject.get("data");
             JSONArray cards = (JSONArray) data.get("cards");
+            JSONArray tokens = (JSONArray) data.get("tokens");
+            JSONObject card;
+            JSONArray subtypes;
 
+            JSONObject identifiers;
+            String primitiveCardName;
+            String primitiveRarity;
+            String side;
             // Metadata header
-            if (createCardsDat) {
-                Metadata.printMetadata(data.get("name"), data.get("releaseDate"), data.get("totalSetSize"), myWriter);
-            }
+            Metadata.printMetadata(data.get("name"), data.get("releaseDate"), data.get("totalSetSize"), myWriter);
 
+//            for (Object tkn : tokens) {
+//                JSONObject token = (JSONObject) tkn;
+//                JSONArray reverseRelated = (JSONArray) token.get("reverseRelated");
+//                for (Object related : reverseRelated) {
+//
+//                    for (Object o : cards) {
+//                        card = (JSONObject) o;
+//                        identifiers = (JSONObject) card.get("identifiers");
+//                        JSONObject tokenIdentifiers = (JSONObject) token.get("identifiers");
+//                        primitiveCardName = (String) card.get("faceName") != null ? (String) card.get("faceName") : (String) card.get("name");
+//                        if (primitiveCardName.equals(related.toString()) && !token.get("name").equals("Copy") && !token.get("name").equals("Energy Reserve") && !token.get("name").equals("Plot") && identifiers.get("multiverseId") != null) {
+//                            CardDat.generateCSV(setCode, identifiers.get("multiverseId") + "t", (String) tokenIdentifiers.get("scryfallId"), myWriterImages, "front/");
+//                        }
+//                    }
+//                }
+//            }
             for (Object o : cards) {
-                JSONObject card = (JSONObject) o;
 
-                JSONObject identifiers = (JSONObject) card.get("identifiers");
-                String primitiveCardName;
-                String primitiveRarity;
-                String side = "front/";
+                card = (JSONObject) o;
+                subtypes = (JSONArray) card.get("subtypes");
 
+                identifiers = (JSONObject) card.get("identifiers");
                 primitiveCardName = (String) card.get("faceName") != null ? (String) card.get("faceName") : (String) card.get("name");
-                primitiveRarity = card.get("side") != null && "b".equals(card.get("side").toString()) ? "T" : (String) card.get("rarity");
-                if (createCardsDat && identifiers.get("multiverseId") != null) {
-
+                //primitiveRarity = card.get("side") != null && "b".equals(card.get("side").toString()) ? "T" : (String) card.get("rarity");
+                primitiveRarity = (String) card.get("rarity");
+                side = card.get("side") != null && "b".equals(card.get("side").toString()) ? "back/" : "front/";
+                if (identifiers.get("multiverseId") != null) {
                     CardDat.generateCardDat(primitiveCardName, identifiers.get("multiverseId"), primitiveRarity, myWriter);
-                    CardDat.generateCSV((String) card.get("setCode"), identifiers.get("multiverseId"), (String) identifiers.get("scryfallId"), myWriterImages, side);
-
-                    continue;
+                    //CardDat.generateCSV(setCode, identifiers.get("multiverseId"), (String) identifiers.get("scryfallId"), myWriterImages, side);
                 }
-                // If card is a reprint, skip it                
+
+                // If card is a reprint, skip it
                 if (card.get("isReprint") != null) {
                     continue;
                 }
                 String nameHeader = "name=" + primitiveCardName;
                 String cardName = primitiveCardName;
-                String oracleText = card.get("text").toString();
+                String oracleText = null;
+                if (card.get("text") != null) {
+                    oracleText = card.get("text").toString();
+                }
                 JSONArray keywords = (JSONArray) card.get("keywords");
                 String manaCost = (String) card.get("manaCost");
                 String mana = "mana=" + manaCost;
@@ -83,6 +92,7 @@ public class JsonParserWagic {
                 String power = "";
                 String toughness = "";
                 String loyalty = "";
+                String colorIndicator = "";
 
                 if (card.get("supertypes") != null) {
                     JSONArray supertypes = (JSONArray) card.get("supertypes");
@@ -100,15 +110,12 @@ public class JsonParserWagic {
                     type += typeStr + " ";
                 }
 
-                if (card.get("subtypes") != null) {
-                    JSONArray subtypes = (JSONArray) card.get("subtypes");
-                    if (!subtypes.isEmpty()) {
-                        subtype = "subtype=";
-                        Iterator subtypesIter = subtypes.iterator();
-                        while (subtypesIter.hasNext()) {
-                            String subtypeStr = (String) subtypesIter.next();
-                            subtype += subtypeStr + " ";
-                        }
+                if (!subtypes.isEmpty()) {
+                    subtype = "subtype=";
+                    Iterator subtypesIter = subtypes.iterator();
+                    while (subtypesIter.hasNext()) {
+                        String subtypeStr = (String) subtypesIter.next();
+                        subtype += subtypeStr + " ";
                     }
                 }
 
@@ -119,6 +126,10 @@ public class JsonParserWagic {
 
                 if (card.get("loyalty") != null) {
                     loyalty = "auto=counter(0/0," + card.get("loyalty") + ",loyalty)";
+                }
+
+                if (card.get("colorIndicator") != null) {
+                    colorIndicator = "color=" + card.get("colorIndicator");
                 }
 
                 // CARD TAG
@@ -133,9 +144,18 @@ public class JsonParserWagic {
                     OracleTextToWagic.parseOracleText(keywords, oracleText, cardName, type, subtype, (String) card.get("power"), manaCost);
                     System.out.println("text=" + oracleText.replace("\n", " -- "));
                 }
-                if (!type.contains("Land")) {
+                if (manaCost != null) {
                     mana = mana.replace("/", "");
                     System.out.println(mana);
+                }
+                if (!colorIndicator.isEmpty()) {
+                    colorIndicator = colorIndicator.replace("W", "white")
+                            .replace("U", "blue").replace("B", "black")
+                            .replace("R", "red").replace("G", "green")
+                            .replace("[", "").replace("]", "")
+                            .replace("\"", "");
+
+                    System.out.println(colorIndicator);
                 }
                 System.out.println(type.trim());
                 if (!subtype.isEmpty()) {
@@ -147,8 +167,6 @@ public class JsonParserWagic {
                 }
                 System.out.println("[/card]\n");
             }
-            myWriter.close();
-            myWriterImages.close();
 
         } catch (FileNotFoundException ex) {
             System.out.println("FileNotFoundException " + ex.getMessage());
--- JsonParserWagic/src/json/parser/wagic/OracleTextToWagic.java
+++ JsonParserWagic/src/json/parser/wagic/OracleTextToWagic.java
@@ -26,8 +26,9 @@ public class OracleTextToWagic {
         // Auto lines
         for (String oracleBit : oracleText.split("\n")) {
             // Permanents, exclude this types
-            if (!(type.contains("Instant") || type.contains("Sorcery") || subtype.contains("Aura") || subtype.contains("Equipment"))) {
+            if (!(type.contains("Instant") || type.contains("Sorcery") || subtype.contains("Aura") || subtype.contains("Equipment") || subtype.contains("Siege"))) {
                 // Evergreen mechanics
+//              autoLineExists(AutoLine.Corrupted(oracleBit, cardName));
                 autoLineExists(AutoEffects.DetermineActivatedAbility(oracleBit, cardName, type, subtype));
                 autoLineExists(AutoLine.ManaAbility(oracleBit, subtype));
                 autoLineExists(AutoLine.Cast(oracleBit, cardName));
@@ -38,45 +39,54 @@ public class OracleTextToWagic {
                 autoLineExists(AutoLine.TakeControl(oracleBit));
                 autoLineExists(AutoLine.CantBeBlockedBy(oracleBit));
                 autoLineExists(AutoLine.Lord(oracleBit, type));
-                autoLineExists(AutoLine.Triggers(oracleBit, cardName, type, subtype, power));
+                autoLineExists(Triggers.processTriggers(oracleBit, cardName, type, subtype, power));
                 autoLineExists(AutoLine.processAsLongAs(oracleBit, cardName));
                 autoLineExists(AutoLine.processForEach(oracleBit, type));
                 autoLineExists(AutoLine.CostReduction(oracleBit));
                 autoLineExists(AutoLineGRN.Surveil(oracleBit));
                 autoLineExists(AutoLine.Prowess(oracleBit));
+                //autoLineExists(AutoLine.Disguise(oracleBit));
+                //autoLineExists(AutoLine.Backup(oracleBit));
 
                 autoLineExists(AutoLineGRN.Proliferate(oracleBit));
-                autoLineExists(AutoLine.Corrupted(oracleBit, cardName));
+                autoLineExists(AutoLineGRN.Convoke(oracleBit));
 
-//                autoLineExists(AutoLine.Unearth(oracleBit));
-//                autoLineExists(AutoLine.Prototype(oracleBit));
-//                autoLineExists(AutoLine.Blitz(oracleBit));
-//                autoLineExists(AutoLineGRN.Kicker(oracleBit, cardName));
-//                autoLineExists(AutoLine.Ninjutsu(oracleBit));
-//                autoLineExists(AutoLineGRN.Undergrowth(oracleBit));
-//                autoLineExists(AutoLineGRN.Convoke(oracleBit));
-//                autoLineExists(AutoLineGRN.Addendum(oracleBit));
-//                autoLineExists(AutoLineGRN.Riot(oracleBit));
-//                autoLineExists(AutoLineGRN.Spectacle(oracleBit));
-//                autoLineExists(AutoLineGRN.Ascend(oracleBit));
-//                autoLineExists(AutoLineGRN.Partner(oracleBit));
-//                autoLineExists(AutoLineGRN.Amass(oracleBit));                
+                autoLineExists(AutoLine.Unearth(oracleBit));
+                autoLineExists(AutoLine.Prototype(oracleBit));
+                autoLineExists(AutoLine.Blitz(oracleBit));
+                autoLineExists(AutoLineGRN.Kicker(oracleBit, cardName));
+                autoLineExists(AutoLine.Ninjutsu(oracleBit));
+                autoLineExists(AutoLineGRN.Undergrowth(oracleBit));
+                autoLineExists(AutoLineGRN.Addendum(oracleBit));
+                autoLineExists(AutoLineGRN.Riot(oracleBit));
+                autoLineExists(AutoLineGRN.Spectacle(oracleBit));
+                autoLineExists(AutoLineGRN.Ascend(oracleBit));
+                autoLineExists(AutoLineGRN.Partner(oracleBit));
+                autoLineExists(AutoLineGRN.Amass(oracleBit));                
             }
             //INSTANT, SORCERY
             if (type.contains("Instant") || type.contains("Sorcery")) {
+                //autoLineExists(AutoLine.Corrupted(oracleBit, cardName));
+                autoLineExists(AutoLine.ChooseOneOrBoth(oracleBit));
                 autoLineExists(AutoLine.MyTarget(oracleBit, "InstantOrSorcery", subtype));
                 autoLineExists(AutoEffects.ProcessEffect(oracleBit, type));
-                //autoLineExists(AutoLine.ExileDestroyDamage(oracleBit, type));
-                //autoLineExists(AutoLine.ReplacerAuraEquipBonus(oracleBit, "InstantOrSorcery"));
-                //autoLineExists(AutoLine.Create(oracleBit));
-                //autoLineExists(AutoLine.PutA(oracleBit, type));
-                autoLineExists(AutoLine.ChooseOneOrBoth(oracleBit));
+                autoLineExists(AutoLine.ExileDestroyDamage(oracleBit, type));
+                autoLineExists(AutoLine.ReplacerAuraEquipBonus(oracleBit, "InstantOrSorcery"));
+                autoLineExists(AutoLine.Create(oracleBit));
+                autoLineExists(AutoLine.PutA(oracleBit, type));
+//                autoLineExists(AutoLine.Casualty(oracleBit, cardName, manaCost));
+                autoLineExists(AutoLineGRN.Surveil(oracleBit));
+                autoLineExists(AutoLineGRN.Convoke(oracleBit));
+
                 //autoLineExists(AutoLineGRN.Kicker(oracleBit, cardName));
                 //autoLineExists(AutoLineGRN.JumpStart(oracleBit, manaCost));
             }
             // AURA
             if (subtype.contains("Aura")) {
+                autoLineExists(AutoLine.Corrupted(oracleBit, cardName));
                 autoLineExists(AutoLine.MyTarget(oracleBit, subtype, subtype));
+                autoLineExists(AutoLineGRN.Convoke(oracleBit));
+
                 //autoLineExists(AutoLine.Triggers(oracleBit, cardName, type, subtype, power));
                 String auraEquipBonus = AutoLine.ReplacerAuraEquipBonus(oracleBit, subtype);
                 if (!auraEquipBonus.isEmpty()) {
@@ -86,6 +96,7 @@ public class OracleTextToWagic {
             // EQUIPMENT
             if (subtype.contains("Equipment")) {
                 //autoLineExists(AutoLine.Triggers(oracleBit, cardName, type, subtype, power));
+                autoLineExists(AutoLineGRN.Convoke(oracleBit));
                 autoLineExists(AutoLine.AuraEquipBonus(oracleBit, "Equipment"));
                 autoLineExists(AutoLine.ForMirrodin(oracleBit, "Equipment"));
                 if (oracleBit.contains("Equip ")) {
@@ -93,9 +104,9 @@ public class OracleTextToWagic {
                 }
             }
             // SAGA
-//            if (subtype.contains("Saga")) {
-//                autoLineExists(AutoLine.EpicSaga(oracleBit, subtype));
-//            }
+            //if (subtype.contains("Saga")) {
+              //  autoLineExists(AutoLine.EpicSaga(oracleBit, subtype));
+            //}
         }
     }
 
