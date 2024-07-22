324e7aea73f756323c751146720772ee26cab2a3
Eduardo MG <punkeduard@gmail.com>
2024-07-21 22:04:16 -0600

    Revert "Before BLB, token images"
    This reverts commit 42866800ce1c9dfa8b2a6768ac6a725491908b32.

--- WagicUtilities-master/src/chat/Opcode.java
+++
@@ -1,8 +0,0 @@
-package chat;
-
-/**************** an interface to define different operation code **************/
-
-public interface Opcode {
-    int CLIENT_CONNECTEING = 1;
-    int CLIENT_CONNECTED = 2;
-}
